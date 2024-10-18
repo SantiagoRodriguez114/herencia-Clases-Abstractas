8bea33d699d2aea81074170932e09ca7701ce6d5
SantiagoRodriguez114 <rodriguezsanti751@gmail.com>
2024-10-18 10:58:04 -0500

    Update Ahorro.java

--- Ejercicio2/src/EjercicioCuentas/Ahorro.java
+++ Ejercicio2/src/EjercicioCuentas/Ahorro.java
@@ -1,34 +1,34 @@
 package EjercicioCuentas;
 
-class Ahorro extends Cuenta {
-    private long interes;
+public class Ahorro extends Cuenta {
+	private long interes;
 
     public Ahorro(int numeroCuenta, String titular) {
         super(numeroCuenta, titular);
-        this.interes = 3; 
+        
     }
 
     @Override
     public void deposito(long valor) {
-        saldo += valor;
         
-        long interesCalculado = saldo * interes / 100;
-        saldo += interesCalculado;
-        System.out.println("Depósito de " + valor + " realizado. Se ha aplicado un interés de " + interesCalculado);
+    	if(valor < 0) {
+    		System.out.println("Valor ingresado no valido...");
+    	} else {
+    	
+    	this.saldo += valor;
+        this.interes = this.saldo * 3 / 100;
+        this.saldo += this.interes;
+        System.out.println("Depósito de " + valor + " realizado. Interés de " + this.interes);
+    	}
     }
 
     @Override
     public void retiro(long valor) {
-        if (valor > saldo) {
+        if (valor > this.saldo) {
             System.out.println("Saldo insuficiente. No se puede realizar el retiro.");
         } else {
-            saldo -= valor;
-            System.out.println("Retiro de " + valor + " realizado. Saldo actual: " + saldo);
+            this.saldo -= valor;
+            System.out.println("Retiro de " + valor + " realizado. Saldo actual: " + this.saldo);
         }
     }
-
-   
-
-    
 }
-
