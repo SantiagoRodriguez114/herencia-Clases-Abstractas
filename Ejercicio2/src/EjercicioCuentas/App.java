0f12b1bd692b785e48cf30c05375ba94542dc030
SantiagoRodriguez114 <rodriguezsanti751@gmail.com>
2024-10-18 10:58:29 -0500

    Update App.java

--- Ejercicio2/src/EjercicioCuentas/App.java
+++ Ejercicio2/src/EjercicioCuentas/App.java
@@ -2,7 +2,7 @@ package EjercicioCuentas;
 
 public class App {
     public static void main(String[] args) {
-        Ahorro cuentaAhorro = new Ahorro(12345, "Juan Pérez");
+        Ahorro cuentaAhorro = new Ahorro(12345, "Pablo");
         System.out.println(cuentaAhorro);
         cuentaAhorro.deposito(1000); 
         System.out.println(cuentaAhorro);
@@ -12,16 +12,16 @@ public class App {
         
         cuentaAhorro.retiro(600);   
         System.out.println(cuentaAhorro);
-        
-        Corriente cuentaCorriente = new Corriente(67890, "Ana Gómez", 500);
-        System.out.println(cuentaCorriente);
-        cuentaCorriente.deposito(1000);
-        System.out.println(cuentaCorriente);
-
-        cuentaCorriente.retiro(1300);
-        System.out.println(cuentaCorriente);
-        
-        cuentaCorriente.retiro(1600);
-        System.out.println(cuentaCorriente);
+      
+//        Corriente cuentaCorriente = new Corriente(67890, "Ana", 500);
+//        System.out.println(cuentaCorriente);
+//        cuentaCorriente.deposito(1000);
+//        System.out.println(cuentaCorriente);
+//
+//        cuentaCorriente.retiro(1300);
+//        System.out.println(cuentaCorriente);
+//        
+//        cuentaCorriente.deposito(300);
+//        System.out.println(cuentaCorriente);
     }        
 }
